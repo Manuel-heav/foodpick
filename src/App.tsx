104bd988f7ffb01747955f0ab56ee5f30d5f8683
manuel-heav <manuelheav32@gmail.com>
2023-11-21 14:52:21 +0300

    Initilaized Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+     <h1 className="text-3xl font-bold underline">
+      Food Pick
+    </h1>
+    </div>
+  )
+}
+
+export default App
