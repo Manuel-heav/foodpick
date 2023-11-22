e24c76a5a53046ce3bbd916c9641e7f4fc3c1490
manuel-heav <manuelheav32@gmail.com>
2023-11-22 20:37:44 +0300

    working on sidebar

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,14 +1,22 @@
 import { SidebarLinks } from "../../constants/links"
-import SidebarLink from "./SidebarLink"
+import { Link, useLocation } from "react-router-dom"
 
 const Sidebar = () => {
+    const { pathname } = useLocation();
   return (
     <nav className="hidden md:flex px-8 py-10 h-100 bg-secondaryColor fixed top-0 left-0 h-screen rounded">
-        <div className="flex gap-10 flex-col">
+        <div className="flex gap-10 flex-col group">
+
             {
-                SidebarLinks.map(link => (
-                    <SidebarLink key={link.label} iconName={link.imgUrl} route={link.route} />
-                ))
+                
+                SidebarLinks.map(link => {
+                    const isActive = pathname === link.route;
+                    return(
+                        <Link key={link.label} to={link.route} className={`p-1.5 rounded-full text-white cursor-pointer group ${isActive && 'bg-mainColor'}`}>
+                            <img src={link.imgUrl} className="h-6 hover:bg-mainColor"/>
+                        </Link>
+                    )
+                })
             }
             
         </div>
