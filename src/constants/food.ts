3f0bfbb2e5acb0e1cf84009ac9fee0a67a4ada44
manuel-heav <manuelheav32@gmail.com>
2023-11-28 11:27:28 +0300

    working on the data

---
+++ src/constants/food.ts
@@ -0,0 +1,38 @@
+const foodTypes = [
+    {
+        place: "Kibnesh",
+        totalFoods: [
+            {food: "Egg Sandwich", price:100 },
+            {food: "ፉል", price:100 }, 
+            {food: "Scrambled Eggs", price:100 },
+            {food: "Firfir", price:100 }, 
+            {food: "Dabo Firfir", price:100 },
+            {food: "Pasta በስጎ", price:100 },  
+            {food: "በያይነት", price:100 }, 
+            {food: "ድንች", price:100 }, 
+            {food: "ምስር", price:100 }, 
+            {food: "ቀይ ወጥ", price: 100},
+            {food: "ምንቸት", price: 100},
+            {food: "ባናቶ", price:100}, 
+            {food: "እርጥብ", price: 100}],
+        totalDrinks: [
+            {drink: "Tea", price:100 },
+            {drink: "Coffee", price:100 },
+            {drink: "ማኪያቶ", price:100 },
+            {drink: "ስፕሪስ", price:100 },
+            {drink: "ቀሽር", price:100 },
+            {drink: "ቀሽር", price:100 },
+        ],
+        lunchOrDinner: [
+            {food: "Pasta በስጎ", price:100 },  
+            {food: "በያይነት", price:100 }, 
+            {food: "ድንች", price:100 }, 
+            {food: "ምስር", price:100 }, 
+            {food: "ቀይ ወጥ", price: 100},
+            {food: "ምንቸት", price: 100},
+            {food: "ባናቶ", price:100}, 
+            {food: "እርጥብ", price: 100}
+        ]
+    }
+]
+
